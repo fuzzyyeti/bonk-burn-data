061f90af68ffd8d0979c012a7b82943ad1913697
Fuzzy Yeti <fuzzyyeti@github.com>
2023-01-15 22:45:23 -0700

    metada and burn cache

---
+++ src/burnFromChain.ts
@@ -0,0 +1,76 @@
+
+import axios from 'axios';
+import dotenv from 'dotenv';
+//const ADMIN='2caMPE832jAJfrbQdGXYtDWyYaqfg2umgh4cN56QgUAG'
+const ADMIN='9Ccz6i4DhCMBzTSkTufRbhkNSd8ySVgns7wJco3cjeU7'
+const BONK_MINT='DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263'
+dotenv.config();
+
+let url = `https://api.helius.xyz/v0/addresses/${ADMIN}/transactions?api-key=${process.env.API_KEY}`
+
+let burnAmounts = {};
+
+
+export const getBurnFromChain = async (nftNumber: number) => {
+    const burnAmount = await getBurnAmount(nftNumber);
+    return burnAmount;
+
+}
+// {
+//     fromTokenAccount: 'J8jn8kjPmqr28MAg9SsP94PCuiYEVxT1R1f2JkA6vaT',
+//     toTokenAccount: '',
+//     fromUserAccount: '2rTVLakADFVbpSdjrm2q8qpBygk6iGGbjdjtQsxRnQVY',
+//     toUserAccount: '',
+//     tokenAmount: 5000000,
+//     mint: 'DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263',
+//     tokenStandard: 'Fungible'
+//   },
+const getBurnAmount = async (searchNumber: Number) => {
+  while (true) {
+    const { data } = await axios.get(url)
+    let x = 0;
+    let last_sig = "";
+    for (const tx of data) {
+      if (tx.type === "NFT_MINT") {
+					const mint = tx.accountData[1].account;
+					if(await isMintNumber(tx.accountData[1].account, searchNumber)) {
+                        const bonkTx = tx.tokenTransfers.filter(
+                            (transfer: BonkTx) => 
+                            transfer.mint === BONK_MINT 
+                            && transfer.toUserAccount === '')[0];
+                        console.log('bonkTx', bonkTx);
+                        if (bonkTx) {
+                            return bonkTx.tokenAmount;
+                        } else {
+                            throw new Error("No bonk burn found");
+                        }
+					}
+          last_sig = tx.signature;
+      }
+    }
+    url = `https://api.helius.xyz/v0/addresses/${ADMIN}/transactions?api-key=${process.env.API_KEY}&before=${last_sig}`
+    if(last_sig === "") {
+      break;
+    }
+    x++;
+  }
+}
+
+const isMintNumber = async (mint: String, searchNumber: Number) =>
+{
+	const url = `https://api.helius.xyz/v0/tokens/metadata?api-key=${process.env.API_KEY}`
+  const { data } = await axios.post(url, { mintAccounts: [mint]})
+  const name = data[0].onChainData.data.name
+	const mintNumber = Number(name.match(/#(\d+)/)[1])
+	console.log('comparing', mintNumber, searchNumber)
+	return mintNumber === searchNumber;
+}
+//parseTransactions();
+//getMetadata('HJ5p3x2TYqBMtLesDFr467XBxQzTmC5PTghsEm2ePoMM')
+//findMintForNumber(10)
+
+type BonkTx = {
+  toUserAccount: string,
+  tokenAmount: Number,
+  mint: string,
+}
--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,13 @@
 import express from 'express';
 import { Request, Response } from 'express';
+import fs from 'fs';
+import { getBurnFromChain } from './burnFromChain';
 
 const base_path = __dirname.split('/').slice(0, -1).join('/');
 const app = express()
 const port = 3000
 const img = 'https://shdw-drive.genesysgo.net/4EVCAGfgf62Dm3bmd2e6L7BnqKZP2jmuXUtufWND12oo/'
+let burnAmounts : null | { [index : string]: number} = null;
 
 app.get('/index.css', (req: Request, res: Response) => {
   res.sendFile('static/index.css', { root: base_path });
@@ -20,6 +23,33 @@ app.get('/:item_num.html', (req: Request, res: Response) => {
   <div class="scroll-container">
   <div id="scroll-text" >5,000,000,000,000 $BONK Burned!</div></div></body></html>`)
 })
+
+app.get('/:item_num.json', async (req: Request, res: Response) => {
+  // read in metadata from the file
+  if (burnAmounts === null) {
+    const burnAmountsRaw = fs.readFileSync(`${base_path}/static/bonkBurned.json`, 'utf8');
+    burnAmounts = JSON.parse(burnAmountsRaw);
+  }
+  if (burnAmounts === null) {
+    throw new Error('could not read bonkBurned.json');
+  }
+  //check if bonkBurnedParsed has item_num as a key
+  let burnAmount = 0;
+  if (!burnAmounts[req.params.item_num]) {
+    const item_number = Number(req.params.item_num)
+    burnAmount = await getBurnFromChain(item_number);
+    burnAmounts[req.params.item_num] = burnAmount;
+    fs.writeFileSync(`${base_path}/static/bonkBurned.json`, JSON.stringify(burnAmounts)); 
+  } else {
+    console.log('setting from cache');
+    burnAmount = burnAmounts[req.params.item_num];
+  }
+  const metadata = fs.readFileSync(`${base_path}/static/metadata/${req.params.item_num}.json`, 'utf8');
+  const parsedMetadata = JSON.parse(metadata);
+  parsedMetadata.attributes = [{ trait_type: 'Bonk Burned', value: burnAmount }, ...parsedMetadata.attributes];
+  res.send(JSON.stringify(parsedMetadata)); 
+  }
+);
 
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`)
