a094c3dda86811bb241e0b524fa8fd4bd76ecdf1
Fuzzy Yeti <fuzzyyeti@github.com>
2023-01-20 22:36:09 -0700

    full update working locally

---
+++ src/getTransaction.ts
@@ -0,0 +1,14 @@
+import axios from 'axios';
+import dotenv from 'dotenv';
+import { getTotalMinted } from './getTotal';
+const BONK_MINT='DezXAZ8z7PnrnRJjz3wXBoRgixCa6xjnB7YaB1pPB263'
+dotenv.config();
+
+const transaction = process.argv[2] 
+
+const testTransaction = async (transaction: string) => {
+	const url = `https://api.helius.xyz/v0/transactions?api-key=${process.env.API_KEY}`
+	const { data } = await axios.post(url, { transactions: [transaction]})
+	console.log(JSON.stringify(data))
+}
+testTransaction(transaction);
